7ce03b1d16b4c515a27d1625ece7281c4121a131
Valeria Casatti <valeria.casatti@gmail.com>
2023-09-12 15:16:13 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import { productsRouter } from "./routes/products.routes.js";
+import { cartsRouter } from "./routes/carts.routes.js";
+
+const port = 8080;
+const app = express();
+
+app.listen(port, () => console.log(`server running in port ${port}`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
